26946499c4fdf9e4a85242f149b5fffff79af0ac
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-09-23 19:56:55 +0900

    Add list2_20

---
+++ src/app/src/test/java/kaleidot725/tests/list2_22/ExceptionAssertionTest.kt
@@ -0,0 +1,19 @@
+package kaleidot725.tests.list2_22
+
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.Test
+import java.lang.RuntimeException
+
+class ExceptionAssertionTest {
+    @Test
+    fun exceptionAssert() {
+        assertThatExceptionOfType(RuntimeException::class.java)
+            .isThrownBy { functionMayThrow() }
+            .withMessage("Aborted!")
+            .withNoCause()
+    }
+
+    fun functionMayThrow() {
+        throw RuntimeException("Aborted!")
+    }
+}
